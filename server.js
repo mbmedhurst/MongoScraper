7280bf63a9d0a5afebe754f41725b65c0d1455d3
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-06-15 20:43:42 -0700

    files created

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const { join } = require('path')
+const app = express()
+// const db = require('mongojs')(news_db)
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+require('./routes')(app)
+
+require('mongoose').connect('mongodb://localhost/wapo_db', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
+  .then(_ => app.listen(3000))
+  .catch(e => console.log(e))
